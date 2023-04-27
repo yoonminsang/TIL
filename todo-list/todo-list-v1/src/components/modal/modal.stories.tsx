cabd736fe5acfe33eba1e420fa1b7a5295f35527
yoonminsnag <apdlyooapdl@naver.com>
2023-04-28 02:26:50 +0900

    feat: 모달컴포넌트 생성

---
+++ todo-list/todo-list-v1/src/components/modal/modal.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Modal } from './modal';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof Modal> = {
+  title: 'Components/modal',
+  component: Modal,
+  //     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  //     tags: ['autodocs'],
+  //     parameters: {
+  //       // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+  //       layout: 'fullscreen',
+  //     },
+};
+
+export default meta;
+type Story = StoryObj<typeof Modal>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    children: <div>모달입니다!</div>,
+  },
+};
---
+++ todo-list/todo-list-v1/src/components/modal/modal.tsx
@@ -0,0 +1,79 @@
+import styled from '@emotion/styled';
+import { MouseEvent, MouseEventHandler, ReactNode } from 'react';
+import { FC } from 'react';
+
+interface Props {
+  visible: boolean;
+  width?: number;
+  height?: number;
+  onClickOverlay?: MouseEventHandler<HTMLDivElement>;
+  children: ReactNode;
+}
+
+export const Modal: FC<Props> = ({
+  visible,
+  width,
+  height,
+  onClickOverlay,
+  children,
+}) => {
+  const onClickModalWrapper = (e: MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      onClickOverlay?.(e);
+    }
+  };
+
+  return (
+    <>
+      <Overlay visible={visible} />
+      <ModalWrapper visible={visible} onClick={onClickModalWrapper}>
+        <ModalInner width={width} height={height}>
+          {children}
+        </ModalInner>
+      </ModalWrapper>
+    </>
+  );
+};
+
+const Overlay = styled.div<{ visible: boolean }>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0);
+  opacity: ${({ visible }) => (visible ? 0.6 : 0)};
+  pointer-events: ${({ visible }) => (visible ? 'all' : 'none')};
+  z-index: 999;
+`;
+
+const ModalWrapper = styled.div<{
+  visible: boolean;
+}>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  opacity: ${(props) => (props.visible ? 1 : 0)};
+  pointer-events: ${(props) => (props.visible ? 'all' : 'none')};
+  z-index: 1000;
+  transform: translateY(${(props) => (props.visible ? '0' : '100%')});
+  transition: 'opacity 300ms ease-in-out, transform 300ms ease-out';
+`;
+
+const ModalInner = styled.div<{
+  width?: number;
+  height?: number;
+}>`
+  background-color: white;
+  width: ${(props) => (props.width ? props.width : '320px')};
+  height: ${(props) => (props.height ? props.height : 'auto')};
+  border-radius: 16px;
+`;
