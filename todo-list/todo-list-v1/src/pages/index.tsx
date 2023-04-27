1865f208f3fa863de4ec9e7d1466cd5bb4f26dd8
yoonminsnag <apdlyooapdl@naver.com>
2023-04-27 23:52:07 +0900

    style: root 페이지에 스타일 적용

--- todo-list/todo-list-v1/src/pages/index.tsx
+++ todo-list/todo-list-v1/src/pages/index.tsx
@@ -1,8 +1,27 @@
+import { css } from '@emotion/react';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
+    <main
+      css={css`
+        h1 {
+          width: 100%;
+          display: flex;
+          justify-content: center;
+        }
+        nav {
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          gap: 30px;
+          a {
+            color: red;
+            text-decoration: none;
+          }
+        }
+      `}
+    >
       <h1>TODO LIST</h1>
       <nav>
         <Link href="/react">상태관리없는 투두리스트</Link>
