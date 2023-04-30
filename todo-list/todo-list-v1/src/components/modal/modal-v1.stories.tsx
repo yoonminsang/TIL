a93cd63e48743f85728ba01face79a90a6f6c719
yoonminsnag <apdlyooapdl@naver.com>
2023-04-30 14:22:27 +0900

    feat: modal v1 컴포넌트 구현

---
+++ todo-list/todo-list-v1/src/components/modal/modal-v1.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ModalV1 } from './modal-v1';
+
+const meta: Meta<typeof ModalV1> = {
+  title: 'Components/Modal/ModalV1',
+  component: ModalV1,
+};
+
+export default meta;
+type Story = StoryObj<typeof ModalV1>;
+
+export const Primary: Story = {
+  args: {
+    visible: true,
+    title: 'title',
+    children: <div>모달입니다!</div>,
+    buttons: [<button key={0}>확인</button>, <button key={1}>취소</button>],
+    footer: 'footer',
+  },
+};
---
+++ todo-list/todo-list-v1/src/components/modal/modal-v1.tsx
@@ -0,0 +1,95 @@
+import { ReactNode } from 'react';
+import { FC } from 'react';
+import { Modal } from './modal';
+import { GetProps } from '@/utils';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Button } from '../button';
+import Image from 'next/image';
+
+interface Props
+  extends Omit<GetProps<typeof Modal>, 'children' | 'width' | 'height'> {
+  width?: GetProps<typeof Modal>['width'];
+  height?: GetProps<typeof Modal>['height'];
+  onClose: () => void;
+  title: ReactNode;
+  children?: ReactNode;
+  footer?: ReactNode;
+  buttons?: ReactNode[];
+}
+
+export const ModalV1: FC<Props> = ({
+  visible,
+  width = '320px',
+  height = 'auto',
+  title,
+  children,
+  footer,
+  buttons,
+  onClose,
+}) => {
+  return (
+    <Modal
+      visible={visible}
+      onClickOverlay={onClose}
+      width={width}
+      height={height}
+    >
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          padding: 16px;
+          header {
+            display: flex;
+            align-items: center;
+            h1 {
+              margin: 0;
+            }
+            button {
+              margin-left: auto;
+            }
+          }
+          footer {
+            .button-wrapper {
+              display: flex;
+              flex-direction: column;
+              gap: 5px;
+            }
+          }
+        `}
+      >
+        <header>
+          <h1>{title}</h1>
+          <Button>
+            <Image src="/close.svg" width={26} height={26} alt="close" />
+          </Button>
+        </header>
+        {children ? (
+          <>
+            <Divider />
+            {children}
+          </>
+        ) : null}
+
+        {footer || buttons ? (
+          <>
+            <Divider />
+            <footer>
+              {footer}
+              <div className="button-wrapper">{buttons}</div>
+            </footer>
+          </>
+        ) : null}
+      </div>
+    </Modal>
+  );
+};
+
+const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  margin-top: 16px;
+  margin-bottom: 16px;
+  background: lightgrey;
+`;
