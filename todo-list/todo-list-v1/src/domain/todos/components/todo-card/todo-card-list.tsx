15910cc953d911126b4d346656f42259515d5ba6
yoonminsnag <apdlyooapdl@naver.com>
2023-05-01 21:59:53 +0900

    feat: TodoCardList에 title 추가

--- todo-list/todo-list-v1/src/domain/todos/components/todo-card/todo-card-list.tsx
+++ todo-list/todo-list-v1/src/domain/todos/components/todo-card/todo-card-list.tsx
@@ -1,10 +1,28 @@
-import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import { FC, ReactNode } from 'react';
 
-export const TodoCardList = styled.li`
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-  list-style-type: none;
-  background-color: #f4f5f7;
-  padding: 1px 5px;
-`;
+interface Props {
+  title: string;
+  children: ReactNode;
+}
+
+export const TodoCardList: FC<Props> = ({ title, children }) => {
+  return (
+    <li
+      css={css`
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+        list-style-type: none;
+        background-color: #f4f5f7;
+        padding: 1px 5px;
+        h2 {
+          padding-left: 10px;
+        }
+      `}
+    >
+      <h2>{title}</h2>
+      {children}
+    </li>
+  );
+};
