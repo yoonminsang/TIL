34c0d1fe911eaa6e644b0441062ab37de51483f6
yoonminsnag <apdlyooapdl@naver.com>
2023-05-01 22:51:40 +0900

    feat: hook으로 get list 데이터 분리

--- todo-list/todo-list-v1/src/pages/recoil/index.tsx
+++ todo-list/todo-list-v1/src/pages/recoil/index.tsx
@@ -2,33 +2,11 @@ import { TodoCard, TodoLayout } from '@/domain/todos';
 import { useGetTodos } from '@/hooks/queries/todo-queries';
 import { TodoStatus, TodoSummaryDto } from '@/mocks/types';
 import { Suspense } from 'react';
+import { useGetList } from './useRecoilPage';
 
 export default function Recoil() {
-  const { isLoading, isError, data } = useGetTodos();
-  console.log(isLoading, isError, data);
-  if (!data || isLoading) return null;
-  const { todo, ing, done } = data.reduce(
-    (acc, cur) => {
-      if (cur.status === TodoStatus.todo) {
-        acc.todo.push(cur);
-      } else if (cur.status === TodoStatus.ing) {
-        acc.ing.push(cur);
-      } else if (cur.status === TodoStatus.done) {
-        acc.done.push(cur);
-      }
-      return acc;
-    },
-    {
-      todo: [] as TodoSummaryDto[],
-      ing: [] as TodoSummaryDto[],
-      done: [] as TodoSummaryDto[],
-    }
-  );
-  const lists = [
-    { title: '할 일', data: todo },
-    { title: '진행중', data: ing },
-    { title: '완료', data: done },
-  ];
+  const { list } = useGetList();
+
   return (
     <Suspense fallback={<div>recoil loading...</div>}>
       <header>
@@ -36,9 +14,9 @@ export default function Recoil() {
       </header>
       <TodoLayout>
         <TodoCard.listContainer>
-          {lists.map(({ title, data }) => (
+          {list.map(({ title, data }) => (
             <TodoCard.list key={title} title={title}>
-              {data.map(({ id, title, priority }) => (
+              {data?.map(({ id, title, priority }) => (
                 <TodoCard key={id} title={title} priority={priority} />
               ))}
             </TodoCard.list>
