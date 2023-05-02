eb052215d2781757221fd17ceaaa8d84f0aae3c7
yoonminsnag <apdlyooapdl@naver.com>
2023-05-02 23:14:20 +0900

    chore: 불필요한 보일러플레이트 제거

--- todo-list/todo-list-v1/src/pages/index.tsx
+++ todo-list/todo-list-v1/src/pages/index.tsx
@@ -1,50 +1,7 @@
-import { Button } from '@/components';
-import { ModalV1 } from '@/components';
-import { useOverlay } from '@/hooks/common';
 import { css } from '@emotion/react';
 import Link from 'next/link';
 
 export default function Home() {
-  // TODO: 나중에 제거
-  const overlay = useOverlay();
-  const openExModal = async () => {
-    const result = await new Promise<boolean>((resolve) => {
-      overlay.open(({ isOpen, close }) => (
-        <ModalV1
-          visible={isOpen}
-          onClose={() => {
-            resolve(false);
-            close();
-          }}
-          title="title"
-          buttons={[
-            <Button
-              key={0}
-              onClick={() => {
-                resolve(true);
-                close();
-              }}
-            >
-              확인
-            </Button>,
-            <Button
-              key={1}
-              onClick={() => {
-                resolve(false);
-                close();
-              }}
-            >
-              취소
-            </Button>,
-          ]}
-        >
-          Children
-        </ModalV1>
-      ));
-    });
-    console.log(result);
-  };
-
   return (
     <main
       css={css`
@@ -65,7 +22,6 @@ export default function Home() {
         }
       `}
     >
-      <button onClick={openExModal}>open modal</button>
       <h1>TODO LIST</h1>
       <nav>
         <Link href="/react">상태관리없는 투두리스트</Link>
