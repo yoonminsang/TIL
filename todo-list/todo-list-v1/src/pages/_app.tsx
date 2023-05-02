d1d9f269160a1f4279e85eba7adab374c8c73440
yoonminsnag <apdlyooapdl@naver.com>
2023-05-03 01:37:26 +0900

    feat: recoil 세팅

--- todo-list/todo-list-v1/src/pages/_app.tsx
+++ todo-list/todo-list-v1/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import { Suspense } from 'react';
+import { RecoilRoot } from 'recoil';
 
 if (process.env.NODE_ENV === 'development') {
   require('../mocks');
@@ -10,13 +11,15 @@ if (process.env.NODE_ENV === 'development') {
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={new QueryClient()}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <OverlayProvider>
-          <Component {...pageProps} />
-        </OverlayProvider>
-      </Suspense>
-      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={new QueryClient()}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <OverlayProvider>
+            <Component {...pageProps} />
+          </OverlayProvider>
+        </Suspense>
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 }
