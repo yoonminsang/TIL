0fdc6979fbf30239004ca871c1a3a37d4d06cdbc
yoonminsnag <apdlyooapdl@naver.com>
2023-05-05 22:34:47 +0900

    feat: react페이지에서 Todos 컴포넌트 제거

--- todo-list/todo-list-v1/src/pages/react/index.tsx
+++ todo-list/todo-list-v1/src/pages/react/index.tsx
@@ -1,16 +1,9 @@
-import { Todos } from '@/domain/todos';
-import { Suspense } from 'react';
 import { Header } from './header';
-import { useGetTodoList } from './useGetTodoList';
 
 export default function React() {
-  const { list } = useGetTodoList();
   return (
     <>
       <Header />
-      <Suspense fallback={<div>recoil loading...</div>}>
-        <Todos list={list} />
-      </Suspense>
     </>
   );
 }
