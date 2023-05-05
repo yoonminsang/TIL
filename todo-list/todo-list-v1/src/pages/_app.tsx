6079cfdaa9d7e89fa7a0901e2fc4c4c1fa2e3f11
yoonminsnag <apdlyooapdl@naver.com>
2023-05-05 22:35:40 +0900

    fix: 페이지가 바뀔때마다 query 캐싱이 초기화되는 이슈 해결

--- todo-list/todo-list-v1/src/pages/_app.tsx
+++ todo-list/todo-list-v1/src/pages/_app.tsx
@@ -2,17 +2,25 @@ import { OverlayProvider } from '@/hooks/common';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
-import { Suspense } from 'react';
-import { RecoilRoot } from 'recoil';
+import { Suspense, useEffect, useState } from 'react';
+import { RecoilRoot, useRecoilSnapshot } from 'recoil';
 
 if (process.env.NODE_ENV === 'development') {
   require('../mocks');
 }
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {},
+      },
+    })
+  );
   return (
     <RecoilRoot>
-      <QueryClientProvider client={new QueryClient()}>
+      <RecoilDebugger />
+      <QueryClientProvider client={queryClient}>
         <Suspense fallback={<div>Loading...</div>}>
           <OverlayProvider>
             <Component {...pageProps} />
