f444332540ae32c8267db0722a73925555a69cab
yoonminsnag <apdlyooapdl@naver.com>
2023-05-06 21:04:45 +0900

    refactor: 사용하지 않는 코드 제거(export, list)

--- todo-list/todo-list-v1/src/hooks/queries/todo-queries.ts
+++ todo-list/todo-list-v1/src/hooks/queries/todo-queries.ts
@@ -1,9 +1,8 @@
 import { todoApi } from '@/apis';
 import { useQuery } from '@tanstack/react-query';
 
-export const TODO_KEY = {
+const TODO_KEY = {
   all: ['todos'] as const,
-  list: () => [...TODO_KEY.all, 'list'] as const,
   detail: (id: number) => [...TODO_KEY.all, id] as const,
 };
 
