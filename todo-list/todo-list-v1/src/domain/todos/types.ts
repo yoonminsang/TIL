7917ef99342b2fe90c8ae0891110cc094f22e706
yoonminsnag <apdlyooapdl@naver.com>
2023-05-07 00:13:34 +0900

    feat: useTodosHeader 훅 생성(create)

--- todo-list/todo-list-v1/src/domain/todos/types.ts
+++ todo-list/todo-list-v1/src/domain/todos/types.ts
@@ -1,6 +1,9 @@
-import { TodoSummaryDto } from '@/mocks/types';
+import { TodoCreateDto, TodoSummaryDto } from '@/mocks/types';
 
 export type UseTodos = () => Return;
+export type UseTodosHeader = () => {
+  handleCreateTodo: (todo: TodoCreateDto) => void;
+};
 
 type Return = {
   filteredTodos: { title: string; data?: TodoSummaryDto[] }[];
--- todo-list/todo-list-v1/src/domain/todos/use-todos.ts
+++ todo-list/todo-list-v1/src/domain/todos/use-todos.ts
@@ -2,7 +2,10 @@ import { useGetTodos } from '@/hooks/queries/todo-queries';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { useEffect } from 'react';
 import { filteredTodosState, todosState } from './atom';
-import { UseTodos } from './types';
+import { UseTodos, UseTodosHeader } from './types';
+import { TodoCreateDto } from '@/mocks/types';
+import { TodoSummaryDto } from '@/mocks/types';
+import { getCreateId } from '@/utils';
 
 export const useTodos: UseTodos = () => {
   const { data } = useGetTodos();
@@ -14,4 +17,22 @@ export const useTodos: UseTodos = () => {
   }, [data, setTodos]);
 
   return { filteredTodos };
+};
+
+export const useTodosHeader: UseTodosHeader = () => {
+  const setTodos = useSetRecoilState(todosState);
+
+  const handleCreateTodo = (todo: TodoCreateDto) => {
+    setTodos((todos) => {
+      const { description, ...todoWithoutDescription } = todo;
+      const newTodo: TodoSummaryDto = {
+        ...todoWithoutDescription,
+        id: getCreateId(todos),
+        createdAt: new Date().toISOString(),
+      };
+      return todos ? [...todos, newTodo] : [newTodo];
+    });
+  };
+
+  return { handleCreateTodo };
 };
