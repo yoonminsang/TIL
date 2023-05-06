812e1f90f97af16d296b80b6588810963dfb1258
yoonminsnag <apdlyooapdl@naver.com>
2023-05-06 20:21:26 +0900

    feat: 유틸타입을 다른 파일로 분리

--- todo-list/todo-list-v1/src/utils/index.ts
+++ todo-list/todo-list-v1/src/utils/index.ts
@@ -1,10 +1,2 @@
-import { ClassAttributes, ComponentClass, JSXElementConstructor } from 'react';
-
-// Infers prop type from component C
-export type GetProps<C> = C extends JSXElementConstructor<infer P>
-  ? C extends ComponentClass<P>
-    ? ClassAttributes<InstanceType<C>> & P
-    : P
-  : never;
-
+export * from './types';
 export * from './errorMessage';
---
+++ todo-list/todo-list-v1/src/utils/types.ts
@@ -0,0 +1,8 @@
+import { ClassAttributes, ComponentClass, JSXElementConstructor } from 'react';
+
+/** Infers prop type from component C */
+export type GetProps<C> = C extends JSXElementConstructor<infer P>
+  ? C extends ComponentClass<P>
+    ? ClassAttributes<InstanceType<C>> & P
+    : P
+  : never;
