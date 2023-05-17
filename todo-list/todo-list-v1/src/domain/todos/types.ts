8eda4ccedb5858dd37fc8baedd4a0afdd5df58fa
yoonminsnag <apdlyooapdl@naver.com>
2023-05-18 01:17:45 +0900

    refactor: 코드정리

--- todo-list/todo-list-v1/src/domain/todos/types.ts
+++ todo-list/todo-list-v1/src/domain/todos/types.ts
@@ -1,10 +1,8 @@
 import { TodoCreateDto, TodoSummaryDto } from '@/mocks/types';
 
-export type UseTodos = () => Return;
+export type UseTodos = () => {
+  filteredTodos: { title: string; data?: TodoSummaryDto[] }[];
+};
 export type UseTodosHeader = () => {
   handleCreateTodo: (todo: TodoCreateDto) => void;
-};
-
-type Return = {
-  filteredTodos: { title: string; data?: TodoSummaryDto[] }[];
 };
