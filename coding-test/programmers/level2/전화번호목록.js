a289fa2fd47cac92d321eda4b2e77c53971501f3
yoonminsnag <apdlyooapdl@naver.com>
2023-07-29 00:37:35 +0900

    전화번호 목록 코딩테스트

---
+++ coding-test/programmers/level2/전화번호목록.js
@@ -0,0 +1,25 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42577
+
+// 정답
+function solution(phone_book) {
+  phone_book.sort();
+  for (let i = 0; i < phone_book.length; i++) {
+    if (phone_book[i]?.startsWith(phone_book[i + 1]) || phone_book[i + 1]?.startsWith(phone_book[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * @Date 2023.07.28
+ */
+function solution(phone_book) {
+  phone_book.sort();
+  for (let i = 0; i < phone_book.length; i++) {
+    if (phone_book[i]?.startsWith(phone_book[i + 1]) || phone_book[i + 1]?.startsWith(phone_book[i])) {
+      return false;
+    }
+  }
+  return true;
+}
