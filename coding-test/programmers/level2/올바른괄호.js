d16c4a4ad0da3eab8aade3164f9af3f9cb2c5a03
yoonminsnag <apdlyooapdl@naver.com>
2023-07-29 00:38:08 +0900

    올바른 괄호 코딩테스트

---
+++ coding-test/programmers/level2/올바른괄호.js
@@ -0,0 +1,33 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+// 풀 가치가 없는 문제
+
+// 정답
+function solution(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      count += 1;
+    } else {
+      count -= 1;
+    }
+    if (count < 0) return false;
+  }
+  return count === 0;
+}
+
+/**
+ * @Date 2023.07.28
+ */
+function solution(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      count += 1;
+    } else {
+      count -= 1;
+    }
+    if (count < 0) return false;
+  }
+  return count === 0;
+}
