2070b5db0f2c2ac517a7a15a2d1913e2c6db47a1
yoonminsnag <apdlyooapdl@naver.com>
2023-07-29 14:47:16 +0900

    코테 H-index

---
+++ coding-test/programmers/level2/H-Index.js
@@ -0,0 +1,29 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42747
+
+// 문제 좀 이상함
+
+// 정답
+function solution(citations) {
+  citations.sort((a, b) => b - a);
+  for (let h = citations.length; h >= 0; h--) {
+    let count;
+    for (count = 0; count < citations.length; count++) {
+      if (h > citations[count]) break;
+    }
+    if (count >= h) return h;
+  }
+}
+
+/**
+ * @Date 2023.07.29
+ */
+function solution(citations) {
+  citations.sort((a, b) => b - a);
+  for (let h = citations.length; h >= 0; h--) {
+    let count;
+    for (count = 0; count < citations.length; count++) {
+      if (h > citations[count]) break;
+    }
+    if (count >= h) return h;
+  }
+}
