3544d06b559db0957443b6f798c4bd04679dc7fa
yoonminsnag <apdlyooapdl@naver.com>
2023-07-29 16:18:12 +0900

    코테 소수찾기

---
+++ coding-test/programmers/level2/소수찾기.js
@@ -0,0 +1,77 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42839
+
+// 정답
+function solution(numbers) {
+  const numbersArr = numbers.split('').map(Number);
+  const set = new Set();
+  for (let i = 1; i <= numbersArr.length; i++) {
+    const permutations = getPermutations(numbersArr, i);
+    permutations.map((permutation) => {
+      const number = Number(permutation.join(''));
+      set.add(number);
+    });
+  }
+  return [...set].reduce((acc, cur) => {
+    if (isPrime(cur)) return acc + 1;
+    return acc;
+  }, 0);
+}
+
+function isPrime(number) {
+  if (number < 2) return false;
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+}
+
+function getPermutations(arr, selectNumber) {
+  if (selectNumber === 1) return arr.map((v) => [v]);
+  const result = [];
+  arr.forEach((fixed, index) => {
+    const rest = [...arr.slice(0, index), ...arr.slice(index + 1)];
+    const permutations = getPermutations(rest, selectNumber - 1);
+    const permutationsWithFixed = permutations.map((permutation) => [fixed, ...permutation]);
+    result.push(...permutationsWithFixed);
+  });
+  return result;
+}
+
+/**
+ * @Date 2023.07.29
+ */
+function solution(numbers) {
+  const numbersArr = numbers.split('').map(Number);
+  const set = new Set();
+  for (let i = 1; i <= numbersArr.length; i++) {
+    const permutations = getPermutations(numbersArr, i);
+    permutations.map((permutation) => {
+      const number = Number(permutation.join(''));
+      set.add(number);
+    });
+  }
+  return [...set].reduce((acc, cur) => {
+    if (isPrime(cur)) return acc + 1;
+    return acc;
+  }, 0);
+}
+
+function isPrime(number) {
+  if (number < 2) return false;
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+}
+
+function getPermutations(arr, selectNumber) {
+  if (selectNumber === 1) return arr.map((v) => [v]);
+  const result = [];
+  arr.forEach((fixed, index) => {
+    const rest = [...arr.slice(0, index), ...arr.slice(index + 1)];
+    const permutations = getPermutations(rest, selectNumber - 1);
+    const permutationsWithFixed = permutations.map((permutation) => [fixed, ...permutation]);
+    result.push(...permutationsWithFixed);
+  });
+  return result;
+}
