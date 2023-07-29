b92fae9e6b925855993fee5d27dc32b65ea26b7b
yoonminsnag <apdlyooapdl@naver.com>
2023-07-30 01:19:06 +0900

    코테 타겟넘버

---
+++ coding-test/programmers/level2/타겟넘버.js
@@ -0,0 +1,35 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/43165
+
+// 정답
+function solution(numbers, target) {
+  let count = 0;
+  dfs(0, 0);
+  return count;
+  function dfs(index, sum) {
+    if (index === numbers.length) {
+      if (sum === target) count += 1;
+      return;
+    }
+    dfs(index + 1, sum + numbers[index]);
+    dfs(index + 1, sum - numbers[index]);
+  }
+}
+
+/**
+ * @Date 2023.07.29
+ */
+{
+  function solution(numbers, target) {
+    let count = 0;
+    dfs(0, 0);
+    return count;
+    function dfs(index, sum) {
+      if (index === numbers.length) {
+        if (sum === target) count += 1;
+        return;
+      }
+      dfs(index + 1, sum + numbers[index]);
+      dfs(index + 1, sum - numbers[index]);
+    }
+  }
+}
