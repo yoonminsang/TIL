bbe21d2c9041443c42cf82a313d46e6d52d4e245
yoonminsnag <apdlyooapdl@naver.com>
2023-08-02 21:04:39 +0900

    feat: local storage와 atom 연결하는 예시코드 추가

--- todo-list/todo-list-v1/src/domain/todos/atom.ts
+++ todo-list/todo-list-v1/src/domain/todos/atom.ts
@@ -1,10 +1,28 @@
-import { atom, selector } from 'recoil';
+import { AtomEffect, atom, selector } from 'recoil';
 import { Todo, TodoStatus } from '@/mocks/types';
 import { errorMessage } from '@/utils';
+import { safeLocalStorage } from '@/lib';
+
+const localStorageEffect: <T>(key: string) => AtomEffect<T> =
+  (key: string) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = safeLocalStorage.get(key);
+    if (savedValue) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue, _, isReset) => {
+      if (isReset) return safeLocalStorage.remove(key);
+
+      return safeLocalStorage.set(key, JSON.stringify(newValue));
+    });
+  };
 
 export const todosState = atom<null | Todo[]>({
   key: 'todosState',
   default: null,
+  effects: [localStorageEffect('todos')],
 });
 
 export const filteredTodosState = selector({
