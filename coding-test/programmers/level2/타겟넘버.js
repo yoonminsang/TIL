230d4bf07b7cab03060fee50b2572717176bc0a5
yoonminsnag <apdlyooapdl@naver.com>
2023-11-13 16:41:49 +0900

    프로그래머스 타겟넘버 dfs 다시풀기

--- coding-test/programmers/level2/타겟넘버.js
+++ coding-test/programmers/level2/타겟넘버.js
@@ -33,3 +33,20 @@ function solution(numbers, target) {
     }
   }
 }
+
+/**
+ * @Date 2023.11.13
+ */
+function solution(numbers, target) {
+  let count = 0;
+  dfs(0, 0);
+  return count;
+  function dfs(index, sum) {
+    if (index >= numbers.length) {
+      if (target === sum) count += 1;
+      return;
+    }
+    dfs(index + 1, sum + numbers[index]);
+    dfs(index + 1, sum - numbers[index]);
+  }
+}
