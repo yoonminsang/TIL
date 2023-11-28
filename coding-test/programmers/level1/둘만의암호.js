e937c3d8aa35724d55c74c1941febeb12b8ad5b9
yoonminsnag <apdlyooapdl@naver.com>
2023-11-29 00:13:04 +0900

    코테 - 둘만의암호

---
+++ coding-test/programmers/level1/둘만의암호.js
@@ -0,0 +1,51 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/155652
+
+// 정답
+function solution(s, skip, index) {
+  const alpabet = 'abcdefghijklmnopqrstuvwxyz';
+  const obj = alpabet.split('').reduce((acc, cur) => {
+    acc[cur] = true;
+    return acc;
+  }, {});
+  for (const v of skip) {
+    delete obj[v];
+  }
+  const skipAlpabetArr = Object.keys(obj);
+
+  return s
+    .split('')
+    .map((v) => {
+      const currentIndex = skipAlpabetArr.findIndex((skipAlpabet) => skipAlpabet === v);
+      const realIndex = (currentIndex + index) % skipAlpabetArr.length;
+      return skipAlpabetArr[realIndex];
+    })
+    .join('');
+}
+
+/**
+ * @Date 2023.11.28
+ */
+
+// 8분
+{
+  function solution(s, skip, index) {
+    const alpabet = 'abcdefghijklmnopqrstuvwxyz';
+    const obj = alpabet.split('').reduce((acc, cur) => {
+      acc[cur] = true;
+      return acc;
+    }, {});
+    for (const v of skip) {
+      delete obj[v];
+    }
+    const skipAlpabetArr = Object.keys(obj);
+
+    return s
+      .split('')
+      .map((v) => {
+        const currentIndex = skipAlpabetArr.findIndex((skipAlpabet) => skipAlpabet === v);
+        const realIndex = (currentIndex + index) % skipAlpabetArr.length;
+        return skipAlpabetArr[realIndex];
+      })
+      .join('');
+  }
+}
