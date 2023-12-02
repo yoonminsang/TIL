fb161940c31a0380032a518ac0fb95f38d0df7ad
yoonminsnag <apdlyooapdl@naver.com>
2023-12-02 16:55:06 +0900

    storage 개선

--- vanilla-js/needs/storage.js
+++ vanilla-js/needs/storage.js
@@ -1,24 +1,111 @@
-const setLocalStorage = (key, data) => {
-  const stringifyData = JSON.stringify(data);
-  localStorage.setItem(key, stringifyData);
-};
+class MemoStorage {
+  storage = new Map();
 
-const getLocalStorage = (key) => {
-  const item = localStorage.getItem(key);
-  return JSON.parse(item);
-};
+  get(key) {
+    return this.storage.get(key) || null;
+  }
 
-/** * 주의점: observer 패턴을 적용하지 않아서 동기화되지 않음. */
-export const useStorage = (key) => {
-  return [
-    getLocalStorage(key),
-    (data) => {
-      setLocalStorage(key, data);
-    },
-  ];
-};
+  set(key, value) {
+    this.storage.set(key, value);
+  }
+
+  remove(key) {
+    this.storage.delete(key);
+  }
+}
+
+class LocalStorage {
+  static canUse() {
+    const TEST_KEY = generateTestKey();
+
+    // 사용자가 쿠키 차단을 하는 경우 LocalStorage '접근' 시에 예외가 발생합니다.
+    try {
+      localStorage.setItem(TEST_KEY, 'test');
+      localStorage.removeItem(TEST_KEY);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  get(key) {
+    return localStorage.getItem(key);
+  }
+
+  set(key, value) {
+    localStorage.setItem(key, value);
+  }
+
+  remove(key) {
+    localStorage.removeItem(key);
+  }
+}
+
+class SessionStorage {
+  static canUse() {
+    const TEST_KEY = generateTestKey();
+
+    // sessionStorage를 사용할 수 없는 경우에 대응합니다.
+    try {
+      sessionStorage.setItem(TEST_KEY, 'test');
+      sessionStorage.removeItem(TEST_KEY);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  get(key) {
+    return sessionStorage.getItem(key);
+  }
+
+  set(key, value) {
+    sessionStorage.setItem(key, value);
+  }
+
+  remove(key) {
+    sessionStorage.removeItem(key);
+  }
+}
+
+function generateTestKey() {
+  return new Array(4)
+    .fill(null)
+    .map(() => Math.random().toString(36).slice(2))
+    .join('');
+}
+
+export function generateStorage() {
+  if (LocalStorage.canUse()) {
+    return new LocalStorage();
+  }
+  return new MemoStorage();
+}
+
+export function generateSessionStorage() {
+  if (SessionStorage.canUse()) {
+    return new SessionStorage();
+  }
+  return new MemoStorage();
+}
+
+export const safeLocalStorage = generateStorage();
+
+export const safeSessionStorage = generateSessionStorage();
 
 /** examples
-export const usePersonalInfoStorage = () => useStorage('personalInfo');
-export const useCardStatusStorage = () => useStorage('cardStatus');
-*/
+class UserStorage {
+  storage = safeLocalStorage();
+  key = 'user';
+
+  get() {
+    this.storage.get(this.key);
+  }
+  set(value) {
+    this.storage.get(this.key, value);
+  }
+  remove() {
+    this.storage.remove(this.key);
+  }
+}
+ */
