153e6311182167283da3235cd10f27430c1d288a
yoonminsnag <apdlyooapdl@naver.com>
2023-12-09 18:04:16 +0900

    feat: 루트페이지 구현

--- twitter/src/app/(beforeLogin)/page.tsx
+++ twitter/src/app/(beforeLogin)/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './page.module.css';
+import zLogo from '../ ../../../../public/zlogo.png';
+
+export default function RootPage() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <>
+      <div className={styles.left}>
+        <Image src={zLogo} alt="logo" />
+      </div>
+      <div className={styles.right}>
+        <h1>지금 일어나고 있는 일</h1>
+        <h2>지금 가입하세요.</h2>
+        <Link href="/i/flow/signup" className={styles.signup}>
+          계정 만들기
+        </Link>
+        <h3>이미 트위터에 가입하셨나요?</h3>
+        <Link href="/login" className={styles.login}>
+          로그인
+        </Link>
+      </div>
+    </>
   );
 }
