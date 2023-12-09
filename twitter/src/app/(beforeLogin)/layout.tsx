6e927d993a87b1041c7f3d83eb53966e4e8c0a58
yoonminsnag <apdlyooapdl@naver.com>
2023-12-09 18:27:04 +0900

    feat: beforelogin layout 스타일 적용

--- twitter/src/app/(beforeLogin)/layout.tsx
+++ twitter/src/app/(beforeLogin)/layout.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from 'react';
+import styles from '@/app/(beforeLogin)/page.module.css';
 
 export default function BeforeLoginLayout({ children, modal }: { children: ReactNode; modal: ReactNode }) {
   return (
-    <>
+    <div className={styles.container}>
       {children}
       {modal}
-    </>
+    </div>
   );
 }
