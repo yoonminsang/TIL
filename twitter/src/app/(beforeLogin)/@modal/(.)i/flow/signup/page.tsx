ed17f37cc65c02193c6aac349ca9edd75aa878c0
yoonminsnag <apdlyooapdl@naver.com>
2023-12-11 20:44:53 +0900

    feat: SignupModal 렌더링

---
+++ twitter/src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignupModal from '@/app/(beforeLogin)/_component/SignupModal';
+
+export default function SignupModalPage() {
+  return (
+    <>
+      난 가로채기지롱 ㅋㅋ
+      <SignupModal />
+    </>
+  );
+}
