df60d74baa4f213d28a37430cdbaf35dbc7e7ff0
yoonminsnag <apdlyooapdl@naver.com>
2023-12-11 22:49:37 +0900

    feat: afterLogin left, right 레이아웃 구현

--- twitter/src/app/(afterLogin)/layout.tsx
+++ twitter/src/app/(afterLogin)/layout.tsx
@@ -1,5 +1,19 @@
 import { ReactNode } from 'react';
+import style from '@/app/(afterLogin)/layout.module.css';
 
 export default function AfterLoginLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return (
+    <div className={style.container}>
+      <header className={style.leftSectionWrapper}>
+        <section className={style.leftSection}></section>
+      </header>
+
+      <div className={style.rightSectionWrapper}>
+        <div className={style.rightSectionInner}>
+          <main className={style.main}>{children}</main>
+          <section className={style.rightSection}></section>
+        </div>
+      </div>
+    </div>
+  );
 }
