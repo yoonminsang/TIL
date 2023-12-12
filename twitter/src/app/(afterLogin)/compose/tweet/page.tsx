1dca48aa9660e3629cc1475c3661f02ad09c26b0
yoonminsnag <apdlyooapdl@naver.com>
2023-12-12 23:35:46 +0900

    feat: tweet페이지에서 새로고침하는경우 모달을 보여주게 설정

--- twitter/src/app/(afterLogin)/compose/tweet/page.tsx
+++ twitter/src/app/(afterLogin)/compose/tweet/page.tsx
@@ -1,3 +1,11 @@
+import TweetModalPage from '../../@modal/(.)compose/tweet/page';
+
 export default function TweetPage() {
-  return <main>게시글</main>;
+  // TODO: 트워터는 새로고침할때 이전 페이지 화면을 배경에 보여줌. lastPageInfo정보를 백엔드나 로컬에 저장해서 처리하는듯함.
+  return (
+    <>
+      여기에 페이지 컴포넌트를 넣으면 돼
+      <TweetModalPage />
+    </>
+  );
 }
