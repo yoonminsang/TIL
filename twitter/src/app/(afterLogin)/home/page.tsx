b295a62002e9419164f2e331f3d30d4a8062eab7
yoonminsnag <apdlyooapdl@naver.com>
2023-12-12 21:25:13 +0900

    feat: home layout에 탭 적용

--- twitter/src/app/(afterLogin)/home/page.tsx
+++ twitter/src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,13 @@
+import TabProvider from './_components/TabProvider';
+import style from './page.module.css';
+import Tab from '@/app/(afterLogin)/home/_components/Tab';
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+      </TabProvider>
+    </main>
+  );
 }
