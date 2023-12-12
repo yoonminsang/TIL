dacfb9de6c670ef8283629babda6ac6d0938e245
yoonminsnag <apdlyooapdl@naver.com>
2023-12-12 21:25:03 +0900

    feat: tab을 useState에서 useContext로 변경

--- twitter/src/app/(afterLogin)/home/_components/Tab.tsx
+++ twitter/src/app/(afterLogin)/home/_components/Tab.tsx
@@ -1,9 +1,10 @@
 'use client';
 import style from './Tab.module.css';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { TabContext } from './TabProvider';
 
 export default function Tab() {
-  const [tab, setTab] = useState('rec');
+  const { tab, setTab } = useContext(TabContext);
 
   const onClickRec = () => {
     setTab('rec');
