1e7d3540c94d77cf7dfb64f936d57350b4531a10
yoonminsnag <apdlyooapdl@naver.com>
2023-12-14 01:05:07 +0900

    feat: Search 페이지 컴포넌트 구현

--- twitter/src/app/(afterLogin)/search/page.tsx
+++ twitter/src/app/(afterLogin)/search/page.tsx
@@ -1,3 +1,40 @@
-export default function SearchPage() {
-  return <main>검색페이지</main>;
+import BackButton from '../_components/BackButton';
+import Post from '../_components/Post';
+import SearchForm from '../_components/SearchForm';
+import Tab from './_components/Tab';
+import style from './page.module.css';
+
+interface Props {
+  searchParams: { q: string; f?: string; pf?: string };
+}
+export default function SearchPage({ searchParams }: Props) {
+  return (
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={searchParams.q} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
+  );
 }
