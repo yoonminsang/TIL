d21b09cebef692bfedfd0b3640344755bdcc1383
yoonminsnag <apdlyooapdl@naver.com>
2023-12-14 00:19:40 +0900

    feat: explore 페이지에 trend 컴포넌트 연결

--- twitter/src/app/(afterLogin)/explore/page.tsx
+++ twitter/src/app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,26 @@
+import SearchForm from '../_components/SearchForm';
+import Trend from '../_components/Trend';
+import style from './page.module.css';
+
 export default function ExplorePage() {
-  return <main>검색페이지</main>;
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
