73630c1c67e5720deac711fdb1dee0dd131cfafc
yoonminsnag <apdlyooapdl@naver.com>
2023-12-14 00:26:45 +0900

    feat: UserNamePage 내부 구현

--- twitter/src/app/(afterLogin)/[username]/page.tsx
+++ twitter/src/app/(afterLogin)/[username]/page.tsx
@@ -1,3 +1,50 @@
+import style from './page.module.css';
+import Post from '@/app/(afterLogin)/_components/Post';
+
 export default function UserNamePage() {
-  return <main>프로필페이지</main>;
+  const user = {
+    id: 'zerohch0',
+    nickname: '제로초',
+    image: '/5Udwvqim.jpg',
+  };
+
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <button className={style.backButton}>
+          <svg
+            width={24}
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03"
+          >
+            <g>
+              <path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"></path>
+            </g>
+          </svg>
+        </button>
+
+        <h3 className={style.headerTitle}>{user.nickname}</h3>
+      </div>
+
+      <div className={style.userZone}>
+        <div className={style.userImage}>
+          <img src={user.image} alt={user.id} />
+        </div>
+        <div className={style.userName}>
+          <div>{user.nickname}</div>
+          <div>@{user.id}</div>
+        </div>
+        <button className={style.followButton}>팔로우</button>
+      </div>
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
+  );
 }
