f09fb1b1666025b7f1e8d0f7638430c9cdcab2fd
yoonminsnag <apdlyooapdl@naver.com>
2023-12-17 17:59:58 +0900

    feat: Post에서 이미지 렌더링하는 코드 추가(임시)

--- twitter/src/app/(afterLogin)/_components/Post.tsx
+++ twitter/src/app/(afterLogin)/_components/Post.tsx
@@ -5,6 +5,7 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ko';
 import ActionButtons from '@/app/(afterLogin)/_components/ActionButtons';
 import PostArticle from './PostArticle';
+import { faker } from '@faker-js/faker';
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime);
@@ -19,8 +20,11 @@ export default function Post() {
     },
     content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
     createdAt: new Date(),
-    Images: [],
+    Images: [] as any,
   };
+  if (Math.random() > 0.5) {
+    target.Images.push({ imageId: 1, link: faker.image.urlLoremFlickr() });
+  }
   return (
     <PostArticle post={target}>
       <div className={style.postWrapper}>
@@ -41,7 +45,16 @@ export default function Post() {
             <span className={style.postDate}>{dayjs(target.createdAt).fromNow(true)}</span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}></div>
+          <div className={style.postImageSection}>
+            {target.Images && target.Images.length > 0 && (
+              <Link
+                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
+                className={style.postImageSection}
+              >
+                <img src={target.Images[0]?.link} alt="" />
+              </Link>
+            )}
+          </div>
           <ActionButtons />
         </div>
       </div>
