b7309c3f2d69bde009e0309b5d61cb92c76d4777
yoonminsnag <apdlyooapdl@naver.com>
2023-12-17 19:21:41 +0900

    feat: Message page 컴포넌트 생성

--- twitter/src/app/(afterLogin)/messages/page.tsx
+++ twitter/src/app/(afterLogin)/messages/page.tsx
@@ -1,3 +1,18 @@
+import Room from './_components/Room';
+import style from './page.module.css';
+
 export default function MessagesPage() {
-  return <main>쪽지페이지</main>;
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </main>
+  );
 }
