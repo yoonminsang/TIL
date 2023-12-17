8701ecc0677f5ffeb4e6bc03b504b1a2cdf7642a
yoonminsnag <apdlyooapdl@naver.com>
2023-12-17 17:39:36 +0900

    feat: StatusById 페이지 ui 작성

--- twitter/src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ twitter/src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,3 +1,28 @@
+import BackButton from '@/app/(afterLogin)/_components/BackButton';
+import Post from '@/app/(afterLogin)/_components/Post';
+import CommentForm from './_components/CommentForm';
+import style from './page.module.css';
+
 export default function StatusByIdPage() {
-  return <main>게시글 상세</main>;
+  return (
+    <div className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>게시하기</h3>
+      </div>
+      <Post />
+      <CommentForm />
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
 }
