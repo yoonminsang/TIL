310b191d21f78aea9cbf6b7490dd32c25ceb4122
yoonminsnag <apdlyooapdl@naver.com>
2023-12-17 18:38:13 +0900

    feat: photo 디테일 모달 생성

---
+++ twitter/src/app/(afterLogin)/@modal/[username]/status/[id]/photo/[photoId]/page.tsx
@@ -0,0 +1,41 @@
+import ActionButtons from '@/app/(afterLogin)/_components/ActionButtons';
+import PhotoModalCloseButton from './_components/PhotoModalCloseButton';
+import style from './page.module.css';
+import { faker } from '@faker-js/faker';
+import Post from '@/app/(afterLogin)/_components/Post';
+import CommentForm from '@/app/(afterLogin)/[username]/status/[id]/_components/CommentForm';
+
+export default function Default() {
+  const photo = {
+    imageId: 1,
+    link: faker.image.urlLoremFlickr(),
+    Post: {
+      content: faker.lorem.text(),
+    },
+  };
+  return (
+    <div className={style.container}>
+      <PhotoModalCloseButton />
+      <div className={style.imageZone}>
+        <img src={photo.link} alt={photo.Post?.content} />
+        <div className={style.image} style={{ backgroundImage: `url(${photo.link})` }} />
+        <div className={style.buttonZone}>
+          <div className={style.buttonInner}>
+            <ActionButtons />
+          </div>
+        </div>
+      </div>
+      <div className={style.commentZone}>
+        <Post />
+        <CommentForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
+}
