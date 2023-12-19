967b7f8d44c6160a991a633d556193716a57fc27
yoonminsnag <apdlyooapdl@naver.com>
2023-12-19 23:44:47 +0900

    feat: BackButton 컴포넌트 분리(클라이언트 컴포넌트 분리)

--- twitter/src/app/(beforeLogin)/_component/SignupModal.tsx
+++ twitter/src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import BackButton from './BackButton';
 import style from './SignupModal.module.css';
 import { useRouter } from 'next/navigation';
 import { ChangeEventHandler, FormEventHandler, useState } from 'react';
@@ -12,10 +13,6 @@ export default function SignupModal() {
   const [imageFile, setImageFile] = useState<File>();
 
   const router = useRouter();
-  const onClickClose = () => {
-    router.back();
-    // TODO: 뒤로가기가 /home이 아니면 /home으로 보내기
-  };
 
   const onChangeId: ChangeEventHandler<HTMLInputElement> = (e) => {
     setId(e.target.value);
@@ -59,18 +56,7 @@ export default function SignupModal() {
       <div className={style.modalBackground}>
         <div className={style.modal}>
           <div className={style.modalHeader}>
-            <button className={style.closeButton} onClick={onClickClose}>
-              <svg
-                width={24}
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-                className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03"
-              >
-                <g>
-                  <path d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
-                </g>
-              </svg>
-            </button>
+            <BackButton />
             <div>계정을 생성하세요.</div>
           </div>
           <form>
