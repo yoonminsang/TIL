d90ce2f550f0582058b2bbc336bcc80ef5affbc1
yoonminsnag <apdlyooapdl@naver.com>
2023-12-27 23:32:36 +0900

    feat: catch all segments router 생성

---
+++ twitter/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,6 @@
+// catch all segments
+// nextpublic을 안붙이면 서버에서만 접근가능함.
+// 붙이면 브라우저에서 접근 가능
+// https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes#catch-all-segments
+
+export { GET, POST } from '@/auth';
