5d7b2023eb9abbbde28a3b26629d1c6ed35a20f4
yoonminsnag <apdlyooapdl@naver.com>
2024-01-04 23:10:46 +0900

    feat: login페이지에서 세션이 있으면 home으로 리다렉트처리

--- twitter/src/app/(beforeLogin)/login/page.tsx
+++ twitter/src/app/(beforeLogin)/login/page.tsx
@@ -2,9 +2,17 @@
 
 import { useRouter } from 'next/navigation';
 import Main from '@/app/(beforeLogin)/_component/Main';
+import { useSession } from 'next-auth/react';
 
 export default function LoginPage() {
   const router = useRouter();
+  const { data: session } = useSession();
+
+  if (session?.user) {
+    router.replace('/home');
+    return null;
+  }
+
   router.replace('/i/flow/login');
   return <Main />;
 }
