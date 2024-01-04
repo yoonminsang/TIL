a19b5b9df6b682de533e0f609272476d1184dd5e
yoonminsnag <apdlyooapdl@naver.com>
2024-01-04 23:10:35 +0900

    feat: 루트페이지에서 세션이 있으면 home으로 리다렉트처리

--- twitter/src/app/(beforeLogin)/page.tsx
+++ twitter/src/app/(beforeLogin)/page.tsx
@@ -1,5 +1,13 @@
 import Main from '@/app/(beforeLogin)/_component/Main';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 
-export default function RootPage() {
+export default async function RootPage() {
+  // useSession의 서버 버전
+  const session = await auth();
+  if (session?.user) {
+    redirect('/home');
+    return null;
+  }
   return <Main />;
 }
