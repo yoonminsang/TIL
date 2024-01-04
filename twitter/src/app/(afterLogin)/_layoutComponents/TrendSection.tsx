f445a1fa23391b68232b39a414c364ad35c7273d
yoonminsnag <apdlyooapdl@naver.com>
2024-01-05 00:09:30 +0900

    feat: 유저가 없을 때 예외적인 ui 표시

--- twitter/src/app/(afterLogin)/_layoutComponents/TrendSection.tsx
+++ twitter/src/app/(afterLogin)/_layoutComponents/TrendSection.tsx
@@ -3,26 +3,35 @@
 import { usePathname } from 'next/navigation';
 import style from './trendSection.module.css';
 import Trend from '@/app/(afterLogin)/_components/Trend';
+import { useSession } from 'next-auth/react';
 
 export default function TrendSection() {
   // 부모 컴포넌트에서 분기를 치는게 더 좋지만 부모가 server component라서 usePathname 훅 사용이 불가능함.
   const pathname = usePathname();
+  const { data } = useSession();
   if (pathname === '/explore') return null;
+  if (data?.user) {
+    return (
+      <div className={style.trendBg}>
+        <div className={style.trend}>
+          <h3>나를 위한 트렌드</h3>
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className={style.trendBg}>
-      <div className={style.trend}>
-        <h3>나를 위한 트렌드</h3>
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-      </div>
+      <div className={style.noTrend}>트렌드를 가져올 수 없습니다.</div>
     </div>
   );
 }
