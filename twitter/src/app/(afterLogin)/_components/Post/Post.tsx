3a87d2f18935bf1be5c9bd56d66f5e8897f7adfc
yoonminsnag <apdlyooapdl@naver.com>
2024-01-13 14:50:22 +0900

    feat: Post 컴포넌트에 post data props 추가

--- twitter/src/app/(afterLogin)/_components/Post/Post.tsx
+++ twitter/src/app/(afterLogin)/_components/Post/Post.tsx
@@ -7,26 +7,18 @@ import ActionButtons from '@/app/(afterLogin)/_components/ActionButtons';
 import PostArticle from './PostArticle';
 import { faker } from '@faker-js/faker';
 import PostImages from './PostImages';
+import { Post } from '@/model/Post';
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime);
 
 interface Props {
   noImage?: boolean;
+  post: Post;
 }
 
-export default function Post({ noImage }: Props) {
-  const target = {
-    postId: 1,
-    User: {
-      id: 'elonmusk',
-      nickname: 'Elon Musk',
-      image: '/yRsRRjGO.jpg',
-    },
-    content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
-    createdAt: new Date(),
-    Images: [] as any,
-  };
+export default function Post({ noImage, post }: Props) {
+  const target = post;
   if (Math.random() > 0.5 && !noImage) {
     target.Images.push(
       { imageId: 1, link: faker.image.urlLoremFlickr() },
