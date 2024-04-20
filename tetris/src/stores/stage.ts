7b17b7b48fb899e645480ac4664e7f60533a4e2d
yoonminsnag <apdlyooapdl@naver.com>
2024-04-20 10:46:20 +0900

    feat: stage store 생성

---
+++ tetris/src/stores/stage.ts
@@ -0,0 +1,25 @@
+import { devtools } from 'zustand/middleware';
+import { create } from 'zustand';
+
+interface State {
+  stage: number;
+}
+
+interface Action {
+  increase: VoidFunction;
+  reset: VoidFunction;
+}
+
+type Integrate = State & Action;
+
+const initialState: State = {
+  stage: 1,
+};
+
+export const useStage = create<Integrate>()(
+  devtools((set) => ({
+    ...initialState,
+    increase: () => set((state) => ({ stage: state.stage + 1 })),
+    reset: () => set(initialState),
+  })),
+);
