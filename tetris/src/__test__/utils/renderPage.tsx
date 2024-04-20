7c1214d23d905a10bcf3a3f78d92d9e8f92fbf63
yoonminsnag <apdlyooapdl@naver.com>
2024-04-20 13:38:55 +0900

    feat: increase 로직이 추가됨에 따라 테스트코드 수정

--- tetris/src/__test__/utils/renderPage.tsx
+++ tetris/src/__test__/utils/renderPage.tsx
@@ -27,19 +27,35 @@ export async function renderRankingPage() {
 }
 
 export async function renderPlayPage(stage?: number) {
-  await renderStageIntroPage(stage);
+  if (stage) {
+    useStage.setState({
+      stage,
+    });
+  }
+  await renderStageIntroPage();
   const button = await screen.findByRole('button', { name: /Go Play Page/ });
   await userEvent.click(button);
 }
 
 export async function renderStageClearPage(stage?: number) {
-  await renderPlayPage(stage);
+  if (stage) {
+    useStage.setState({
+      // NOTE: stage를 clear하면 1단계가 오르기 때문에 -1을 해줌
+      stage: stage - 1,
+    });
+  }
+  await renderPlayPage();
   const button = await screen.findByRole('button', { name: /Go Stage Clear Page/ });
   await userEvent.click(button);
 }
 
 export async function renderDeadPage(stage?: number) {
-  await renderPlayPage(stage);
+  if (stage) {
+    useStage.setState({
+      stage,
+    });
+  }
+  await renderPlayPage();
   const button = await screen.findByRole('button', { name: /Go Stage Dead Page/ });
   await userEvent.click(button);
 }
