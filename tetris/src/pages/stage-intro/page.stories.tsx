18f8921e22a466bb7373be197bab30339131ef56
yoonminsnag <apdlyooapdl@naver.com>
2024-04-23 00:32:29 +0900

    feat: 모든 페이지에 스토리 작성

---
+++ tetris/src/pages/stage-intro/page.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import StageIntroPage from './page';
+
+const meta = {
+  title: 'Pages/StageIntroPage',
+  component: StageIntroPage,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+  args: {
+    onChangePlayPage: fn(),
+  },
+} satisfies Meta<typeof StageIntroPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    stage: 1,
+  },
+};
