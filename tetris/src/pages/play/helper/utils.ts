584949988f71737065e2fa9f14b9b02969ef2b5d
yoonminsnag <apdlyooapdl@naver.com>
2024-04-24 00:07:28 +0900

    feat: rotateClockWiseIn2DArr 함수 생성

--- tetris/src/pages/play/helper/utils.ts
+++ tetris/src/pages/play/helper/utils.ts
@@ -2,3 +2,14 @@ export const getRandom = (maxNumber: number, random = Math.random()) => {
   return Math.floor(random * maxNumber);
 };
 
+export const rotateClockWiseIn2DArr = <T>(arr: T[][]) => {
+  const arrColLength = arr.length;
+  const arrRowLength = arr[0].length;
+  const result = [...Array(arrRowLength)].map(() => Array(arrColLength).fill(undefined));
+  arr.forEach((colArr, col) => {
+    colArr.forEach((v, row) => {
+      result[row][arrColLength - col - 1] = v;
+    });
+  });
+  return result;
+};
