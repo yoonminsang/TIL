03a1e811f63ecbc09ea81df1c4d2d5979f1bcc64
yoonminsnag <apdlyooapdl@naver.com>
2024-04-24 00:08:27 +0900

    feat: BLOCK_MAP 생성

---
+++ tetris/src/pages/play/helper/block.ts
@@ -0,0 +1,56 @@
+import { Block, BlockType } from './types';
+
+export const BLOCK_MAP: Record<BlockType, Block> = {
+  i: {
+    type: 'i',
+    shape: [
+      [false, true, false, false],
+      [false, true, false, false],
+      [false, true, false, false],
+      [false, true, false, false],
+    ],
+  },
+  o: {
+    type: 'o',
+    shape: [
+      [true, true],
+      [true, true],
+    ],
+  },
+  l: {
+    type: 'l',
+    shape: [
+      [false, false, true],
+      [true, true, true],
+    ],
+  },
+  j: {
+    type: 'j',
+    shape: [
+      [true, false, false],
+      [true, true, true],
+    ],
+  },
+  t: {
+    type: 't',
+    shape: [
+      [false, true, false],
+      [true, true, true],
+    ],
+  },
+  s: {
+    type: 's',
+    shape: [
+      [false, true, true],
+      [true, true, false],
+    ],
+  },
+  z: {
+    type: 'z',
+    shape: [
+      [true, true, false],
+      [false, true, true],
+    ],
+  },
+};
+
