31f5142d1b07439c0046191d27b6e29436b00bfe
yoonminsnag <apdlyooapdl@naver.com>
2024-05-01 17:18:43 +0900

    feat: getGoalClearLine 함수 생성

--- tetris/src/pages/play/helper/index.ts
+++ tetris/src/pages/play/helper/index.ts
@@ -1,4 +1,5 @@
 export { getRandomBlock, getBlockMaxSize } from './block';
+export { getGoalClearLine } from './clearLine';
 export { SETTINGS } from './constants';
 export { getGameSpeed } from './speed';
 export { combineBlockToTable, getEmptyTable, findCompletedLines } from './table';
