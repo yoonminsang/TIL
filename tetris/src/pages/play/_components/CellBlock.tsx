c851dc4f3cfab60f727b28a85ae7b1a32d303fb9
yoonminsnag <apdlyooapdl@naver.com>
2024-05-18 15:00:41 +0900

    style: CellBlock 스타일 변경

--- tetris/src/pages/play/_components/CellBlock.tsx
+++ tetris/src/pages/play/_components/CellBlock.tsx
@@ -1,37 +1,37 @@
 import { Cell } from '../helper';
+import colors from 'tailwindcss/colors';
 
 interface CellProps {
   blockType: Cell;
 }
 
 function CellBlock({ blockType }: CellProps) {
+  const style = getBlockStyle(blockType);
   return (
-    <div className="w-[20px] h-[20px]" style={{ backgroundColor: getBlocStyle(blockType), border: '1px solid gray' }} />
+    <div className="relative h-[20px] w-[20px] border border-gray-500" style={{ backgroundColor: style.background }}>
+      <div className="absolute inset-0 m-0.5 border border-gray-700" style={{ background: style.gradient }} />
+    </div>
   );
 }
 
 export default CellBlock;
 
-const getBlocStyle = (cell: Cell) => {
-  switch (cell) {
-    case 'i':
-      return 'red';
-    case 'o':
-      return 'orange';
-    case 'l':
-      return 'yellow';
-    case 'j':
-      return 'green';
-    case 's':
-      return 'blue';
-    case 't':
-      return 'indigo';
-    case 'z':
-      return 'purple';
-    case 'shadow':
-      return 'lightgray';
-    case null:
-    default:
-      return undefined;
-  }
+const colorMap = {
+  i: colors.red[500],
+  o: colors.orange[500],
+  l: colors.yellow[500],
+  j: colors.green[500],
+  s: colors.blue[500],
+  t: colors.indigo[500],
+  z: colors.purple[500],
+  shadow: colors.gray[400],
+  empty: colors.gray[900],
+} satisfies Record<string, string>;
+
+const getBlockStyle = (cell: Cell) => {
+  const baseColor = colorMap[cell ?? 'empty'];
+  return {
+    background: baseColor,
+    gradient: `linear-gradient(to top right, ${baseColor}, ${colors.gray[900]})`,
+  };
 };
