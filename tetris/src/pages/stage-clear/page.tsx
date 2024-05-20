c13d5768c3edfa5756abef4e2a8f34ab4f3fb5ae
yoonminsnag <apdlyooapdl@naver.com>
2024-05-21 00:22:56 +0900

    fix: stage clear에서 잘못된 stage를 보여주는 버그 수정

--- tetris/src/pages/stage-clear/page.tsx
+++ tetris/src/pages/stage-clear/page.tsx
@@ -11,7 +11,8 @@ export default function StageClearPage({ stage, onChangeStageIntroPage }: Props)
     <RootLayout>
       <div className="flex flex-col items-center justify-center gap-[16px]">
         <h1 className="text-2xl">StageClearPage</h1>
-        <div className="text-l">{stage} Stage Clear</div>
+        {/* NOTE: state를 clear했기 때문에 stage가 이미 +1되어있다. 그렇기 때문에 이전 stage 데이터를 보여줘야함 */}
+        <div className="text-l">{stage - 1} Stage Clear</div>
         <Button onClick={onChangeStageIntroPage}>Go Stage Intro Page</Button>
       </div>
     </RootLayout>
