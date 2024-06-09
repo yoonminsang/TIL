416a235b9aa1fa67887871b90d1ca69b7aae01e8
yoonminsnag <apdlyooapdl@naver.com>
2024-06-09 16:44:03 +0900

    feat: lint와 prettier를 통합하는 라이브러리 설치

--- tetris/.eslintrc.cjs
+++ tetris/.eslintrc.cjs
@@ -1,14 +1,18 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+    /** https://github.com/prettier/eslint-plugin-prettier#recommended-configuration */
+    'plugin:prettier/recommended',
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
