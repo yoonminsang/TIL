5f843f14b9bd8fdaa7c07e5cf1a21578763a97b1
yoonminsnag <apdlyooapdl@naver.com>
2024-07-04 00:03:50 +0900

    feat: UI 컴포넌트에 변경된 타입 적용

--- tetris/src/pages/play/_components/BlockRenderer.tsx
+++ tetris/src/pages/play/_components/BlockRenderer.tsx
@@ -11,7 +11,7 @@ function BlockRenderer({ cellList }: BlockRendererProps) {
       {cellList.map((col, colIndex) => (
         <div className="flex" key={colIndex}>
           {col.map((row, rowIndex) => (
-            <CellBlock blockType={row} hasBorder={false} key={rowIndex} />
+            <CellBlock blockType={row.type} hasBorder={false} key={rowIndex} />
           ))}
         </div>
       ))}
