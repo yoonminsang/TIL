bcf4e69505375138decbca5598ec930eaf76e891
yoonminsnag <apdlyooapdl@naver.com>
2024-07-14 17:48:10 +0900

    feat: TableRenderer에 clearLine이 있을때 애니메이션 추가

--- tetris/src/pages/play/_components/TableRenderer.tsx
+++ tetris/src/pages/play/_components/TableRenderer.tsx
@@ -1,13 +1,16 @@
+import { cn } from '@/lib/utils';
 import { Table } from '../helper';
 import CellBlock from './CellBlock';
+import styles from './TableRenderer.module.css';
 
 interface TableRendererProps {
   cellList: Table;
+  clearLineArr: number[];
 }
 
-function TableRenderer({ cellList }: TableRendererProps) {
+function TableRenderer({ cellList, clearLineArr }: TableRendererProps) {
   return (
-    <div>
+    <div className={cn('relative', clearLineArr.length > 0 && styles['animate-clear-line'])}>
       {cellList.map((col, colIndex) => (
         <div className="flex" key={colIndex}>
           {col.map((row, rowIndex) => (
