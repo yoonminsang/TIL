ef503852f46d5634dd47d5b32852baebb347eb7f
yoonminsnag <apdlyooapdl@naver.com>
2024-07-15 23:19:54 +0900

    feat: boards controller와 service에 모델 타입 적용

--- nestjs-board-app/src/boards/boards.controller.ts
+++ nestjs-board-app/src/boards/boards.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
   @Get()
-  getAllBoars() {
+  getAllBoars(): Board[] {
     return this.boardService.getAllBoards();
   }
 }
