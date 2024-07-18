9c4f6bc7056126fe65a004de3971c72ee8e637c0
yoonminsnag <apdlyooapdl@naver.com>
2024-07-19 01:39:35 +0900

    feat: board service 테스트 코드 작성

--- nestjs-board-app/src/modules/boards/boards.service.spec.ts
+++ nestjs-board-app/src/modules/boards/boards.service.spec.ts
@@ -1,14 +1,51 @@
+import { ConfigModule } from '@nestjs/config';
 import type { TestingModule } from '@nestjs/testing';
 import { Test } from '@nestjs/testing';
 
+import { BoardRepository } from './board.repository';
 import { BoardsService } from './boards.service';
+
+import type { IBoards } from '@/api-interfaces';
+import { BoardStatus } from '@/api-interfaces';
+import envConfig from '@/configs/env.config';
+import { Board } from '@/entities/board.entity';
+
+class StubBoardRepository {
+  private boards: Board[] = [];
+
+  async find() {
+    return this.boards;
+  }
+
+  async create(board: Omit<Board, 'id'>) {
+    const created = { ...board, id: this.boards.length + 1 };
+    this.boards.push(created);
+    return created;
+  }
+
+  save() {}
+}
 
 describe('BoardsService', () => {
   let service: BoardsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BoardsService],
+      imports: [
+        ConfigModule.forRoot({
+          cache: true,
+          isGlobal: true,
+          envFilePath: `.env.${process.env.NODE_ENV}`,
+          load: [envConfig],
+        }),
+      ],
+      providers: [
+        BoardsService,
+        {
+          provide: BoardRepository,
+          useClass: StubBoardRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<BoardsService>(BoardsService);
@@ -16,5 +53,17 @@ describe('BoardsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a board', async () => {
+    const createBoardDto: IBoards.CreateBoardBodyDto = { title: 'Test', description: 'Test desc' };
+    const board = new Board();
+    board.title = createBoardDto.title;
+    board.description = createBoardDto.description;
+    board.status = BoardStatus.PUBLIC;
+
+    const result = await service.createBoard(createBoardDto);
+    expect(result.title).toBe(board.title);
+    expect(result.description).toEqual(board.description);
   });
 });
