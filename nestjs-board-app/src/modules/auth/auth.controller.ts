c20f0f4dbe7e70f19292528b62239e287f45436c
yoonminsnag <apdlyooapdl@naver.com>
2024-07-21 01:31:08 +0900

    feat: 유저를 가져오는 커스텀 데코레이터 생성

--- nestjs-board-app/src/modules/auth/auth.controller.ts
+++ nestjs-board-app/src/modules/auth/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
+import { GetUser } from './user.decorator';
 
 import { IAuth } from '@/api-interfaces/structures/auth.structure';
+import { User } from '@/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +19,12 @@ export class AuthController {
   @Post('/signin')
   signIn(@Body() authCredentialsDto: IAuth.SignInBodyDto): Promise<IAuth.SignInResDto> {
     return this.authService.signIn(authCredentialsDto);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  // test(@Req() req) {
+  test(@GetUser() user: User) {
+    console.log('req.user', user);
   }
 }
