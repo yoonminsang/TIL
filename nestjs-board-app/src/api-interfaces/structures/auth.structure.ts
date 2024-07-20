53ed95d302af777f19af968a3bc2a46f4992659f
yoonminsnag <apdlyooapdl@naver.com>
2024-07-20 14:14:44 +0900

    feat: AuthCredentialsDto에 class validator 추가

--- nestjs-board-app/src/api-interfaces/structures/auth.structure.ts
+++ nestjs-board-app/src/api-interfaces/structures/auth.structure.ts
@@ -1,12 +1,21 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+import { AuthPolicyValue } from '../constants';
 
 export namespace IAuth {
   export class AuthCredentialsDto {
     @IsString()
+    @MinLength(AuthPolicyValue.MIN_USER_LENGTH)
+    @MaxLength(AuthPolicyValue.MAX_USER_LENGTH)
     @IsNotEmpty()
     username!: string;
 
     @IsString()
+    @MinLength(AuthPolicyValue.MIN_PASSWORD_LENGTH)
+    @MaxLength(AuthPolicyValue.MAX_PASSWORD_LENGTH)
+    @Matches(AuthPolicyValue.PASSWORD_REGEX, {
+      message: 'password only accepts english and number',
+    })
     @IsNotEmpty()
     password!: string;
   }
