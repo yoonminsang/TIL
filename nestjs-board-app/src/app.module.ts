3dcd0017059aaf45f48548efb207442db9367143
yoonminsnag <apdlyooapdl@naver.com>
2024-07-20 12:33:26 +0900

    feat: app 모듈 cli로 생성

--- nestjs-board-app/src/app.module.ts
+++ nestjs-board-app/src/app.module.ts
@@ -3,10 +3,11 @@ import { APP_PIPE } from '@nestjs/core';
 
 import { getEnvConfigModule } from './configs/env.config';
 import { getTypeORMConfigModule } from './configs/typeorm.config';
+import { AuthModule } from './modules/auth/auth.module';
 import { BoardsModule } from './modules/boards/boards.module';
 
 @Module({
-  imports: [getEnvConfigModule(), getTypeORMConfigModule(), BoardsModule],
+  imports: [getEnvConfigModule(), getTypeORMConfigModule(), BoardsModule, AuthModule],
   providers: [
     {
       provide: APP_PIPE,
