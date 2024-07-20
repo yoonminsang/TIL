bd91069c4470a3cfcda397674d9a99179a6b7bb3
yoonminsnag <apdlyooapdl@naver.com>
2024-07-21 01:55:51 +0900

    feat: user repository 테스트코드 작성

---
+++ nestjs-board-app/src/modules/auth/user.repository.spec.ts
@@ -0,0 +1,71 @@
+import type { TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UserRepository } from './user.repository';
+
+import { Board } from '@/entities/board.entity';
+import { getAppConfigModuleForTest } from '@/utils/test.util';
+
+describe('UserRepository', () => {
+  let repository: UserRepository;
+  let moduleRef: TestingModule;
+
+  beforeAll(async () => {
+    moduleRef = await Test.createTestingModule({
+      imports: [...getAppConfigModuleForTest(), TypeOrmModule.forFeature([Board])],
+      providers: [UserRepository],
+    }).compile();
+
+    repository = moduleRef.get<UserRepository>(UserRepository);
+  });
+
+  afterAll(async () => {
+    await repository.clear();
+    await moduleRef.close();
+  });
+
+  beforeEach(async () => {
+    await repository.clear();
+  });
+
+  it('should be defined', () => {
+    expect(repository).toBeDefined();
+  });
+
+  it('should create user', async () => {
+    const user = repository.create({
+      username: 'username',
+      password: 'password',
+    });
+
+    await repository.save(user);
+
+    const savedUser = await repository.findOne({
+      where: { id: user.id },
+    });
+    expect(savedUser).toBeDefined();
+    expect(savedUser?.username).toEqual('username');
+  });
+
+  it('should fail create user by username duplicate', async () => {
+    try {
+      const user = repository.create({
+        username: 'username',
+        password: 'password',
+      });
+
+      await repository.save(user);
+
+      const user2 = repository.create({
+        username: 'username',
+        password: 'password2',
+      });
+
+      await repository.save(user2);
+      expect(1).toBe(2);
+    } catch (err: any) {
+      expect(err.code).toBe('ER_DUP_ENTRY');
+    }
+  });
+});
