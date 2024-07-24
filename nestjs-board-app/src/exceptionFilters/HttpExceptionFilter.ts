6a8ec6adfe03cfcd4b6acb7798ee413ea25e34a8
yoonminsnag <apdlyooapdl@naver.com>
2024-07-25 01:26:26 +0900

    feat: http에러가 아닌 에러들도 로그를 찍게 변경합니다

--- nestjs-board-app/src/exceptionFilters/HttpExceptionFilter.ts
+++ nestjs-board-app/src/exceptionFilters/HttpExceptionFilter.ts
@@ -3,26 +3,43 @@ import { Request, Response } from 'express';
 
 import { AccessTokenPayload } from '@/modules/auth/auth.dto';
 
-@Catch(HttpException)
+@Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
   private readonly logger: Logger = new Logger();
 
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
     const req = ctx.getRequest<Request>();
     const { body, user, originalUrl, method, ip } = req;
     const userAgent = req.get('user-agent');
     const userId = (user as AccessTokenPayload)?.id;
-    const statusCode = exception.getStatus();
+    if (exception instanceof HttpException) {
+      const statusCode = exception.getStatus();
 
-    const message = `USER-${userId} ${method} ${originalUrl} ${JSON.stringify(body)} ${statusCode} ${ip} ${userAgent}`;
-    if (statusCode >= HttpStatus.INTERNAL_SERVER_ERROR) {
-      this.logger.error(message);
+      const message = `USER-${userId} ${method} ${originalUrl} ${JSON.stringify(body)} ${statusCode} ${ip} ${userAgent}`;
+      if (statusCode >= HttpStatus.INTERNAL_SERVER_ERROR) {
+        this.logger.error(message, { exception: this.formatError(exception) });
+      } else {
+        this.logger.warn(message, { exception: this.formatError(exception) });
+      }
+
+      res.status(statusCode).json(exception.getResponse());
     } else {
-      this.logger.warn(message);
-    }
+      const message = `로직 에러. USER-${userId} ${method} ${originalUrl} ${JSON.stringify(body)} ${HttpStatus.INTERNAL_SERVER_ERROR} ${ip} ${userAgent}`;
+      this.logger.error(message, { exception: this.formatError(exception) });
 
-    res.status(statusCode).json(exception.getResponse());
+      res.status(500).json({ statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: 'Internal server error' });
+    }
+  }
+
+  private formatError(exception: unknown): object {
+    if (exception instanceof Error) {
+      return {
+        message: exception.message,
+        stack: exception.stack,
+      };
+    }
+    return { exception };
   }
 }
