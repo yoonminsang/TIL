de95f92308b102aed5ef067c01c3453af29d65ed
yoonminsnag <apdlyooapdl@naver.com>
2024-08-06 23:06:29 +0900

    feat: user 여부에 따른 세부적인 guard 생성 및 테스트용 api 생성

--- nestjs-board-app-firebase/src/modules/firebase/firebase.guard.ts
+++ nestjs-board-app-firebase/src/modules/firebase/firebase.guard.ts
@@ -28,3 +28,59 @@ export class FirebaseAuthGuard implements CanActivate {
     }
   }
 }
+
+@Injectable()
+export class OptionalFirebaseAuthGuard implements CanActivate {
+  constructor(
+    private readonly firebaseService: FirebaseService,
+    private readonly logger: Logger
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = extractToken(request);
+
+    if (!token) {
+      return true;
+    }
+
+    try {
+      const user = await this.firebaseService.getUserByToken(token);
+      request.user = user;
+    } catch (error) {
+      this.logger.warn('OptionalFirebaseAuthGuard에서 getUserByToken 에러 발생', { error });
+      return true;
+    }
+
+    return true;
+  }
+}
+
+@Injectable()
+export class NoUserFirebaseAuthGuard implements CanActivate {
+  constructor(
+    private readonly firebaseService: FirebaseService,
+    private readonly logger: Logger
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = extractToken(request);
+
+    if (!token) {
+      return true;
+    }
+
+    try {
+      const user = await this.firebaseService.getUserByToken(token);
+      if (user) {
+        return false;
+      }
+    } catch (error) {
+      this.logger.warn('NoUserFirebaseAuthGuard에서 getUserByToken 에러 발생', { error });
+      return true;
+    }
+
+    return true;
+  }
+}
--- nestjs-board-app-firebase/src/modules/users/users.controller.ts
+++ nestjs-board-app-firebase/src/modules/users/users.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 
-import { FirebaseAuthGuard } from '../firebase/firebase.guard';
+import { FirebaseAuthGuard, NoUserFirebaseAuthGuard, OptionalFirebaseAuthGuard } from '../firebase/firebase.guard';
 
 import { GetUser } from './user.decorator';
 import { UsersService } from './users.service';
@@ -29,15 +29,15 @@ export class UsersController {
     return { user };
   }
 
-  // TODO: SetMetadata 공식문서 읽고 가드와 데코레이터 구현하기
-
-  // TODO: 토큰이 없거나 올바르지 않은 토큰이 있어야 성공하는 케이스
+  // 토큰이 없거나 올바르지 않은 토큰이 있어야 성공하는 케이스
+  @UseGuards(NoUserFirebaseAuthGuard)
   @Get('/token-test-2')
   tokenTest2(@GetUser() user: IUser.User) {
     return { user };
   }
 
-  // TODO: 토큰이 있던 없던 req.user에 user를 넣어주는 케이스
+  // 토큰이 있던 없던 req.user에 user를 넣어주는 케이스
+  @UseGuards(OptionalFirebaseAuthGuard)
   @Get('/token-test-3')
   tokenTest3(@GetUser() user: IUser.User) {
     return { user };
