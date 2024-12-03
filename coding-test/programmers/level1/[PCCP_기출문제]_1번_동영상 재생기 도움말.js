15116d2a306092b339c261dd67796445f3c33cda
yoonminsnag <apdlyooapdl@naver.com>
2024-12-03 23:58:04 +0900

    코테 쉬운 시간 문제

---
+++ coding-test/programmers/level1/[PCCP_기출문제]_1번_동영상 재생기 도움말.js
@@ -0,0 +1,53 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/340213?language=javascript
+{
+  const NEXT = 'next';
+  const PREV = 'prev';
+  function solution(video_len, pos, op_start, op_end, commands) {
+    const videoLenToSecond = timeToSecond(video_len);
+    let postToSecond = timeToSecond(pos);
+    const opStartToSecond = timeToSecond(op_start);
+    const opEndToSecond = timeToSecond(op_end);
+
+    if (isOpening(postToSecond)) {
+      postToSecond = opEndToSecond;
+    }
+
+    const queue = [...commands];
+    while (commands.length) {
+      const currentCommand = commands.shift();
+      if (currentCommand === NEXT) {
+        postToSecond = Math.min(postToSecond + 10, videoLenToSecond);
+      } else {
+        postToSecond = Math.max(postToSecond - 10, 0);
+      }
+      if (isOpening(postToSecond)) {
+        postToSecond = opEndToSecond;
+      }
+    }
+    return secondToTime(postToSecond);
+
+    function timeToSecond(timeData) {
+      const [minute, second] = timeData.split(':').map(Number);
+      return minute * 60 + second;
+    }
+
+    function secondToTime(secondData) {
+      let minute = Math.floor(secondData / 60);
+      if (minute < 10) {
+        minute = `0${minute}`;
+      }
+      let second = secondData % 60;
+      if (second < 10) {
+        second = `0${second}`;
+      }
+      return `${minute}:${second}`;
+    }
+
+    function isOpening(secondData) {
+      if (opStartToSecond <= secondData && opEndToSecond >= secondData) {
+        return true;
+      }
+      return false;
+    }
+  }
+}
