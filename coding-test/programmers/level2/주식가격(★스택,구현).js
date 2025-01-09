6d25a428983ec26f228828af95646c2b30ffd4f3
yoonminsnag <apdlyooapdl@naver.com>
2025-01-09 14:22:53 +0900

    코테 주식가격

--- coding-test/programmers/level2/주식가격(★스택,구현).js
+++ coding-test/programmers/level2/주식가격(★스택,구현).js
@@ -41,3 +41,26 @@ function solution(prices) {
     return answer;
   }
 }
+
+// 잘 생각이 안나서 가장 직관적인 이중 루프로 품. prices길이는 10^5이라서 n^2이면 10^10이고 100초 걸림. 문제 잘못만든듯. 이건 시간복잡도 초과 떠야됌. 스택으로 풀어야 시간복잡도 충족함
+/**
+ * @Date 2025.01.09
+ */
+{
+  function solution(prices) {
+    const result = [];
+    for (let i = 0; i < prices.length; i++) {
+      const currentPrice = prices[i];
+      const initialJ = i + 1;
+      let j = initialJ;
+      for (j; j < prices.length; j++) {
+        if (currentPrice > prices[j]) {
+          j += 1;
+          break;
+        }
+      }
+      result.push(j - initialJ);
+    }
+    return result;
+  }
+}
