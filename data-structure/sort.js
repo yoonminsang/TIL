4e780de459224c964eb8ae8dbe292964473ba5c7
yoonminsnag <apdlyooapdl@naver.com>
2025-02-04 00:54:37 +0900

    정렬

--- data-structure/sort.js
+++ data-structure/sort.js
@@ -12,6 +12,15 @@ function bubbleSort(arr) {
   }
   return arr;
 }
+
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+    }
+  }
+  return arr;
+}
 console.log(bubbleSort([6, 5, 4, 3, 2, 1])); // [1,2,3,4,5,6]
 
 /**
@@ -49,6 +58,17 @@ function insertionSort(arr) {
   }
   return arr;
 }
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    const temp = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = temp;
+  }
+}
 console.log(insertionSort([6, 5, 4, 3, 2, 1])); // [1,2,3,4,5,6]
 
 /**
@@ -83,8 +103,8 @@ console.log(mergeSort([6, 5, 4, 3, 2, 1])); // [1,2,3,4,5,6]
  * @description 기준점을 획득한 다음 해당 기준점을 기준으로 배열을 나눈다.
     1. 배열의 중간 지점에 위치한 원소(피봇)을 선택
     2. 2개의 포인터를 생성. 
-      2-1. 피봇보다 더 작은 원소가 나올 때까지 좌측 포인터를 움직임
-      2-2. 피봇보다 더 큰 원소가 나올 때까지 우측 포인터를 움직인 다음,
+      2-1. 좌측 포인터에 해당하는 값이 피봇보다 큰 값을 찾을 때 까지 좌측 포인터를 움직임(즉 최종 index는 피봇보다 커짐)
+      2-2. 우측 포인터에 해당하는 값이 피봇보다 작은 값을 찾을 때 까지 우측 포인터를 움직인 다음,(즉 최종 index는 피봇보다 작아짐)
       2-3. 두 포인터에 해당하는 원소를 서로 교환한다.
       2-4. 이 과정을 좌측 포인터가 우측 포인터보다 더 커질 때까지 반복
       이로써 피봇보다 작은 원소는 좌측에, 큰 원소는 우측에 나열
@@ -93,39 +113,60 @@ console.log(mergeSort([6, 5, 4, 3, 2, 1])); // [1,2,3,4,5,6]
  * @param {number[]} arr
  * @returns arr
  */
-function quickSort(arr) {
-  return quickSortHelper(arr, 0, arr.length - 1);
-  function quickSortHelper(arr, leftIndex, rightIndex) {
-    if (arr.length <= 1) return arr;
+function quickSort(arr, leftIndex = 0, rightIndex = arr.length - 1) {
+  if (arr.length <= 1) return arr;
 
-    const index = partition(arr, leftIndex, rightIndex);
-    // 왼쪽 부분 배열에 대한 정렬을 수행하기 때문에 index-1을 해줌
-    if (leftIndex < index - 1) quickSortHelper(arr, leftIndex, index - 1);
-    if (index < rightIndex) quickSortHelper(arr, index, rightIndex);
-    return arr;
-  }
-  function partition(arr, _leftIndex, _rightIndex) {
-    let [leftIndex, rightIndex] = [_leftIndex, _rightIndex];
-    const pivotIndex = Math.floor((leftIndex + rightIndex) / 2);
-    const pivot = arr[pivotIndex];
-
-    // 2-4. 이 과정을 좌측 포인터가 우측 포인터보다 더 커질 때까지 반복
-    while (leftIndex <= rightIndex) {
-      // 2-1. 피봇보다 더 작은 원소가 나올 때까지 좌측 포인터를 움직임
-      while (pivot > arr[leftIndex]) leftIndex += 1;
-      // 2-2. 피봇보다 더 큰 원소가 나올 때까지 우측 포인터를 움직인 다음,
-      while (pivot < arr[rightIndex]) rightIndex -= 1;
-
-      if (leftIndex <= rightIndex) {
-        // 2-3. 두 포인터에 해당하는 원소를 서로 교환한다.
-        swap(arr, leftIndex, rightIndex);
-        leftIndex += 1;
-        rightIndex -= 1;
-      }
-    }
-    return leftIndex;
-  }
+  const index = partition(arr, leftIndex, rightIndex);
+  // 왼쪽 부분 배열에 대한 정렬을 수행하기 때문에 index-1을 해줌
+  if (leftIndex < index - 1) quickSort(arr, leftIndex, index - 1);
+  if (index < rightIndex) quickSort(arr, index, rightIndex);
+  return arr;
 }
+function partition(arr, leftIndex, rightIndex) {
+  const pivotIndex = Math.floor((leftIndex + rightIndex) / 2);
+  const pivot = arr[pivotIndex];
+
+  // 2-4. 이 과정을 좌측 포인터가 우측 포인터보다 더 커질 때까지 반복
+  while (leftIndex <= rightIndex) {
+    // 2-1. 좌측 포인터에 해당하는 값이 피봇보다 큰 값을 찾을 때 까지 좌측 포인터를 움직임(즉 최종 index는 피봇보다 커짐)
+    while (arr[leftIndex] < pivot) leftIndex += 1;
+    // 2-2. 우측 포인터에 해당하는 값이 피봇보다 작은 값을 찾을 때 까지 우측 포인터를 움직인 다음,(즉 최종 index는 피봇보다 작아짐)
+    while (arr[rightIndex] > pivot) rightIndex -= 1;
+
+    if (leftIndex <= rightIndex) {
+      // 2-3. 두 포인터에 해당하는 원소를 서로 교환한다.
+      swap(arr, leftIndex, rightIndex);
+      leftIndex += 1;
+      rightIndex -= 1;
+    }
+  }
+  return leftIndex;
+}
+
+// In-Place: 기존 객체를 변경
+// Out-Place: 새로운 객체 생성
+// 아웃 플레이스. 덜 효율적. 위 코드는 인플레이스라서 효율적임)
+function quickSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const leftArr = [];
+  const rightArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === Math.floor(arr.length / 2)) {
+      continue;
+    }
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+}
+
 console.log(quickSort([6, 5, 4, 3, 2, 1])); // [1,2,3,4,5,6]
 
 function countSort() {}
