e7f0bfbf231ddaf56cb4b929b9fcdba07dc8dbfc
yoonminsnag <apdlyooapdl@naver.com>
2025-02-25 13:01:59 +0900

    useRef 구현

--- blog/함수형_리액트/index.js
+++ blog/함수형_리액트/index.js
@@ -365,6 +365,108 @@
     effectsIndex = 0;
   }
 
-  render();
+  // render();
 }
 // useState와 접근 방법이 크게 다르지 않다.
+// deps의 내용이 이전과 변경되었는지 확인하고 변경된 경우에만 effect를 실행한다.
+
+// 다섯번째 목표: useRef 구현하기
+{
+  let states = [];
+  let statesIndex = 0;
+
+  function useState(initialState) {
+    if (states.length === statesIndex) {
+      states.push(initialState);
+    }
+
+    const currentIndex = statesIndex;
+    const state = states[currentIndex];
+    const setState = (nextState) => {
+      states[currentIndex] = nextState;
+      render();
+    };
+
+    statesIndex += 1;
+    return [state, setState];
+  }
+
+  let effects = [];
+  let effectsIndex = 0;
+
+  function useEffect(effect, deps) {
+    const currentIndex = effectsIndex; // 오래된 클로저문제 방지
+    const hasNoDeps = !deps;
+    const prevDeps = effects[currentIndex];
+
+    const hasChangedDeps = !prevDeps || deps.some((dep, i) => !Object.is(dep, prevDeps[i]));
+
+    if (hasNoDeps || hasChangedDeps) {
+      effect();
+      effects[currentIndex] = deps;
+    }
+
+    effectsIndex += 1;
+  }
+
+  let refs = [];
+  let refsIndex = 0;
+
+  function useRef(initialValue) {
+    if (refs.length === refsIndex) {
+      refs.push({ current: initialValue });
+    }
+
+    const ref = refs[refsIndex];
+    refsIndex += 1;
+    return ref;
+  }
+
+  function App() {
+    const [input, setInput] = useState('');
+    const countRef = useRef(0);
+
+    console.log('count: ', countRef.current);
+    console.log('input: ', input);
+
+    window.increase = () => (countRef.current = countRef.current + 1);
+    window.decrease = () => (countRef.current = countRef.current - 1);
+    window.changeInput = () => setInput(document.querySelector('input').value);
+
+    window.render = render;
+
+    useEffect(() => {
+      console.log('useEffect: count changed', countRef.current);
+    }, [countRef.current]);
+    useEffect(() => {
+      console.log('useEffect: input changed', input);
+    }, [input]);
+    return /*html*/ `
+      <div>
+        count: ${countRef.current}  
+        <button onclick="increase()">increase</button>
+        <button onclick="decrease(event)">decrease</button>
+        <button onclick="render()">rerender</button>
+      </div>
+      <div>
+        input: ${input}
+        <input oninput="changeInput()"/>
+      </div>
+      `;
+  }
+
+  function render() {
+    const $root = document.getElementById('root');
+    $root.innerHTML = `
+      ${App()}
+      `;
+    statesIndex = 0;
+    effectsIndex = 0;
+    refsIndex = 0;
+  }
+
+  render();
+}
+// useRef도 useState와 크게 다른게 없다.
+// setState 대신 ref.current를 이용해서 상태를 변경하는 부분만 다르다.
+// html에 ref를 적용하는건 v-dom을 구현해야 할 수 있기 때문에 일단은 생략한다.
