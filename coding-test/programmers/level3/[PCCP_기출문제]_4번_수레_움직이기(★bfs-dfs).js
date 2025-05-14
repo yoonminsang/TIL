cf928a7cf7bb03f55a9858f299f552e6853cfe90
yoonminsang <apdlyooapdl@gmail.com>
2025-05-14 22:46:10 +0900

    코테 dfs

--- coding-test/programmers/level3/[PCCP_기출문제]_4번_수레_움직이기(★bfs-dfs).js
+++ coding-test/programmers/level3/[PCCP_기출문제]_4번_수레_움직이기(★bfs-dfs).js
@@ -1,6 +1,7 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/250134
 
 // bfs로 푸는게 좀 빡셌다.
+// dfs가 훨씬 쉬움
 
 /**
  * @Date 2025.05.14
@@ -180,6 +181,128 @@
   }
 }
 
-// bfs 리팩터링
-
 // dfs + 백트래킹
+{
+  const dy = [-1, 0, 1, 0];
+  const dx = [0, 1, 0, -1];
+
+  // 수레는 벽이나 격자 판 밖으로 움직일 수 없습니다.
+  // 수레는 자신이 방문했던 칸으로 움직일 수 없습니다.
+  // 자신의 도착 칸에 위치한 수레는 움직이지 않습니다. 계속 해당 칸에 고정해 놓아야 합니다.
+  // 동시에 두 수레를 같은 칸으로 움직일 수 없습니다.
+  // 수레끼리 자리를 바꾸며 움직일 수 없습니다.
+
+  // maze[i][j]	의미
+  // 0	빈칸
+  // 1	빨간 수레의 시작 칸
+  // 2	파란 수레의 시작 칸
+  // 3	빨간 수레의 도착 칸
+  // 4	파란 수레의 도착 칸
+  // 5	벽
+  const MAZE_VALUE = {
+    empty: 0,
+    redStart: 1,
+    blueStart: 2,
+    redEnd: 3,
+    blueEnd: 4,
+    wall: 5,
+  };
+
+  function solution(maze) {
+    const colLength = maze.length;
+    const rowLength = maze[0].length;
+
+    const { redStart, blueStart } = getInitialPosition();
+
+    const redVisited = Array(colLength)
+      .fill(null)
+      .map(() => Array(rowLength).fill(false));
+    const blueVisited = Array(colLength)
+      .fill(null)
+      .map(() => Array(rowLength).fill(false));
+
+    const countSet = new Set();
+
+    redVisited[redStart[0]][redStart[1]] = true;
+    blueVisited[blueStart[0]][blueStart[1]] = true;
+    dfs(redStart[0], redStart[1], blueStart[0], blueStart[1], 0);
+
+    if (countSet.size > 0) {
+      return Math.min(...countSet);
+    } else {
+      return 0;
+    }
+
+    function getInitialPosition() {
+      let redStart;
+      let blueStart;
+      maze.forEach((mazeCol, y) => {
+        mazeCol.forEach((v, x) => {
+          if (v === MAZE_VALUE.redStart) {
+            redStart = [y, x];
+          } else if (v === MAZE_VALUE.blueStart) {
+            blueStart = [y, x];
+          }
+        });
+      });
+      if (!redStart || !blueStart) {
+        throw new Error('start, end 변수 할당 실패');
+      }
+      return { redStart, blueStart };
+    }
+
+    function dfs(redY, redX, blueY, blueX, count) {
+      if (maze[redY][redX] === MAZE_VALUE.redEnd && maze[blueY][blueX] === MAZE_VALUE.blueEnd) {
+        countSet.add(count);
+        return count;
+      }
+
+      for (let i = 0; i < 4; i++) {
+        const isRedCompleted = maze[redY][redX] === MAZE_VALUE.redEnd;
+        const nextRedY = isRedCompleted ? redY : redY + dy[i];
+        const nextRedX = isRedCompleted ? redX : redX + dx[i];
+        if (!isRedCompleted) {
+          if (nextRedX < 0 || nextRedY < 0 || nextRedX >= rowLength || nextRedY >= colLength) continue;
+          if (maze[nextRedY][nextRedX] === MAZE_VALUE.wall) continue;
+          if (redVisited[nextRedY][nextRedX]) continue;
+        }
+        for (let j = 0; j < 4; j++) {
+          const isBlueCompleted = maze[blueY][blueX] === MAZE_VALUE.blueEnd;
+          const nextBlueY = isBlueCompleted ? blueY : blueY + dy[j];
+          const nextBlueX = isBlueCompleted ? blueX : blueX + dx[j];
+          if (!isBlueCompleted) {
+            if (nextBlueX < 0 || nextBlueY < 0 || nextBlueX >= rowLength || nextBlueY >= colLength) continue;
+            if (maze[nextBlueY][nextBlueX] === MAZE_VALUE.wall) continue;
+            if (blueVisited[nextBlueY][nextBlueX]) continue;
+          }
+          if (nextRedY === nextBlueY && nextRedX === nextBlueX) continue;
+          if (nextRedY === blueY && nextRedX === blueX && nextBlueY === redY && nextBlueX === redX) continue;
+          redVisited[nextRedY][nextRedX] = true;
+          blueVisited[nextBlueY][nextBlueX] = true;
+          dfs(nextRedY, nextRedX, nextBlueY, nextBlueX, count + 1);
+          redVisited[nextRedY][nextRedX] = false;
+          blueVisited[nextBlueY][nextBlueX] = false;
+        }
+      }
+    }
+  }
+
+  console.log(
+    solution([
+      [1, 4],
+      [0, 0],
+      [2, 3],
+    ]),
+  );
+  // 3
+
+  console.log(
+    solution([
+      [1, 0, 2],
+      [0, 0, 0],
+      [5, 0, 5],
+      [4, 0, 3],
+    ]),
+  );
+  // 7
+}
