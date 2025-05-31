29ad447350c5e6e39311d40d759466a799b6d9fb
yoonminsang <apdlyooapdl@gmail.com>
2025-05-31 17:20:30 +0900

    n queen 코테

--- coding-test/programmers/level2/N-Queen.js
+++ coding-test/programmers/level2/N-Queen.js
@@ -37,3 +37,37 @@
     }
   }
 }
+
+/**
+ * @Date 2025.05.31
+ */
+{
+  function solution(n) {
+    // col에 있는 row 인덱스배열. ex)[2,0,3,1], [1,3,0,2]
+    const queens = [];
+    let count = 0;
+    dfs(0);
+    return count;
+    function dfs(col) {
+      if (col === n) {
+        count += 1;
+        return;
+      }
+      for (let i = 0; i < n; i++) {
+        if (isValid(i, col)) {
+          queens[col] = i;
+          dfs(col + 1);
+        }
+      }
+    }
+    function isValid(row, col) {
+      // 세로가 같은 경우는 dfs 백트래킹이라서 들어오지 않음
+      for (let i = 0; i < col; i++) {
+        const isHorizontal = queens[i] === row;
+        const isDiagonal = Math.abs(queens[i] - row) === Math.abs(i - col);
+        if (isHorizontal || isDiagonal) return false;
+      }
+      return true;
+    }
+  }
+}
